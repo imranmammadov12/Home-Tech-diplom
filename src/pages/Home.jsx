227af416178bc0342ded4d36cd64fd2ff374d4f0
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-24 23:34:06 +0400

    working on hero section of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
 import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img.png'
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
-  return (
-    <Helmet title={"Home"}></Helmet>
-  )
+
+  const year = new Date().getFullYear()
+
+
+
+  return <Helmet title={'Home '} >
+  <section className="hero__section">
+    <Container>
+      <Row>
+        <Col lg='6' md='6'>
+          <div className="hero__content">
+             <p className="hero__subtitle">Trending product in {year}</p>
+             <h2>Make Your Interior More Minimalistic & Modern </h2>
+             <p>Step into a world of simplicity and sophistication.
+               Discover minimalist designs that redefine modern living.
+                Your home, reimagined.</p>
+
+              <motion.button  whileTap={{scale: 1.2}} className="buy__btn"><Link to='/shop'>SHOP NOW</Link></motion.button>
+          </div>
+        </Col>
+
+        <Col lg='6' md='6'>
+          <div className="hero__img">
+            <img src={heroImg} alt="" />
+          </div>
+        </Col>
+
+      </Row>
+    </Container>
+  </section>
+  </Helmet>
 }
 
-export default Home
+export default Home;
