52cf6ff2fd866d696e064eba7180762285365832
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-25 18:18:54 +0400

    adding deleteItem to cartSlice plus additionally adding localStorage

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,10 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    cartItems: [],
-    totalAmount: 0,
-    totalQuantity: 0
-}
+const loadState = () => {
+    const serializedState = localStorage.getItem('cart');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  };
+  
+  const saveState = (state) => {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('cart', serializedState);
+  };
+const initialState = loadState() || {
+  cartItems: [],
+  totalAmount: 0,
+  totalQuantity: 0,
+};
 
 
 const cartSlice = createSlice({
@@ -13,15 +25,15 @@ const cartSlice = createSlice({
     reducers: {
         addItem:(state, action) => {
             const newItem = action.payload
-            const existingItem = state.cartItems.find((item)=> item.id === newItem.id);
+            const existingItemIndex = state.cartItems.find((item)=> item.id === newItem.id);
 
             state.totalQuantity++;
 
-            if(!existingItem){
+            if(existingItemIndex === -1) {
                 state.cartItems.push({
                     id: newItem.id,
                     productName: newItem.productName,
-                    image: newItem.imgUrl,
+                    imgUrl: newItem.imgUrl,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price
@@ -30,16 +42,30 @@ const cartSlice = createSlice({
 
 
             else{
-                existingItem.quantity++
-                existingItem.totalPrice = Number(existingItem.totalPrice) + Number(newItem.price);
+                state.cartItems[existingItemIndex].quantity++;
+                state.cartItems[existingItemIndex].totalPrice += newItem.price;
             }
 
-            state.totalAmount = state.cartItems.reduce((total,item)=> total + Number(item.price) * Number(item.quantity));
-            console.log(newItem);
-        }
+            state.totalQuantity++;
+            state.totalAmount = state.cartItems.reduce((total, item) => total + Number(item.totalPrice), 0);
 
+            saveState(state);
+        },
+        deleteItem: (state, action) => {
+            const id = action.payload;
+            const existingItemIndex = state.cartItems.findIndex(item => item.id === id);
+      
+            if (existingItemIndex !== -1) {
+              state.totalQuantity -= state.cartItems[existingItemIndex].quantity;
+              state.totalAmount -= state.cartItems[existingItemIndex].totalPrice;
+      
+              state.cartItems.splice(existingItemIndex, 1);
+            }
+      
+            saveState(state);
+          },
     }
-})
+});
 
 export const  cartActions  = cartSlice.actions;
 
