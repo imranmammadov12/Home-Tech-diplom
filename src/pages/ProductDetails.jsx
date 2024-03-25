f3422542634ca6a26ff9fdd9150fdc85d2ec59a9
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-25 15:59:36 +0400

    added info about product in page ProductDetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from 'react';
+import {Container, Row, Col} from 'reactstrap';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/CommonSection';
+import {motion} from 'framer-motion';
+import { useParams } from 'react-router-dom';
+import products from '../assets/data/products';
 
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
+
+  const {id} = useParams()
+  const product = products.find(item => item.id === id);
+  
+  const {imgUrl, productName, price, avgRating, reviews, description, shortDesc , category} = product;
+
+
+
+
+  return  (
+      <Helmet title={productName}>
+        <CommonSection title={productName} />
+          <section className='pt-0'>
+            <Container>
+              <Row>
+                <Col lg='6'>
+                  <img src={imgUrl} alt="" />
+                </Col>
+  
+                <Col lg='6'>
+                  <div className="product__details">
+                    <h2>{productName}</h2>
+                    <div className="product__rating d-flex align-items-center gap-5 mb-3">
+                      <div>
+                        <span><i class="ri-star-s-fill"></i></span>
+                        <span><i class="ri-star-s-fill"></i></span>
+                        <span><i class="ri-star-s-fill"></i></span>
+                        <span><i class="ri-star-s-fill"></i></span>
+                        <span><i class="ri-star-half-fill"></i></span>
+                      </div>
+  
+                        <p>(<span>{avgRating}</span> ratings)</p>
+                    </div>
+  
+                    <div className='d-flex align-items-center gap-5'>
+                    <span className='product__price'>${price}</span>
+                    <span>Category :  {category.toUpperCase()}</span>
+                    </div>
+                    <p className='mt-3'>{shortDesc}</p>
+  
+                    <motion.button whileTap={{scale: 1.2}} className="buy__btn">Add to Cart</motion.button>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+      </Helmet>
   )
 }
 
