2e1b742a30b68de57250c275586fd01f096b9998
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-25 15:13:43 +0400

    adding CommonSection to Shop and Cart pages

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/CommonSection';
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <Helmet title='Cart'>
+        <CommonSection title='Shopping Cart' />
+    </Helmet>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import CommonSection from '../components/UI/CommonSection';
+import Helmet from '../components/Helmet/Helmet'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <Helmet title='Shop'>
+      <CommonSection title='Products' />
+    </Helmet>
   )
 }
 
