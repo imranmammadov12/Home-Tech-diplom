e1b329eef492076b9296280380cd51c7fc6e89e3
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-25 15:25:12 +0400

    adding on the shop page the ability to filter by category and sort by price, as well as adding a search by product plus adding styles (mobile versions too)

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
 
 import CommonSection from '../components/UI/CommonSection';
-import Helmet from '../components/Helmet/Helmet'
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from 'reactstrap';
+import '../styles/shop.css'
+
 
 const Shop = () => {
   return (
     <Helmet title='Shop'>
       <CommonSection title='Products' />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3' md='6'>
+              <div className="filter__widget">
+                <select>
+                <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md='6' className='text-end'>
+            <div className="filter__widget">
+                <select>
+                <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='6' md='12'>
+              <div className="search__box">
+                <input type="text" placeholder='Search.....' />
+                <span><i class="ri-search-2-line"></i></span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
     </Helmet>
   )
 }
